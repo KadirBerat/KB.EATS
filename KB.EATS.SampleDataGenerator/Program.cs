db0cf0e9af7955ff8bc2b0486e531566f7969d12
Kadir Berat Güventürk <kadirguventurk@outlook.com>
2023-10-27 10:40:02 +0300

    Add project files.

---
+++ KB.EATS.SampleDataGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KB.EATS.SampleDataGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
